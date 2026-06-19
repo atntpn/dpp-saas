8c1f47c81579d76815d3547bb542d96aab12a290
atntpn <cinemas-grenadine9s@icloud.com>
2026-06-20 00:45:43 +0200

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.vercel.app',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
